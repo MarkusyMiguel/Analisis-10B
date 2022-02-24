be71dcc2f97b5388b967abc1536395f5682aaeaa
MarkusyMiguel <youismyfoukingdream@gmail.com>
2022-02-24 07:47:45 -0500

    Tallersito

---
+++ Index.js
@@ -0,0 +1,45 @@
+console.log ("Hola Mundo");//imprimir por consola
+//comentario en linea   
+/*comentario
+en
+varias
+lineas
+o
+multilinea
+*/ 
+/*son espacios de memoria que almacenen la informacion
+existen varios tipos de variables en js, pero generalmente se utilizan los siguientes:
+string: Almacena cadenas de texto, se debe aclarar o almacenar el valor entre comillas 
+int: Almacena numeros enteros 
+Double: Almacena numeros reales 
+boolean: Almacenan valores de verdad (true o false)
+*/
+/* Declarar variables
+Declarar una variable es crearla, decirle al lenguaje como se llama la variable eh indicarle que ya existe.
+Para declarar una variable se puede utilizar ya sea la palabra reservada var o let
+*/
+var nombre;
+
+console.log(nombre);
+/* inicializar variables
+Para inicializar variables es necesario asignar un valor cuando se crea esta variable
+*/
+var apellido = "Scholz";
+console.log(apellido);
+/* asignar un valor:
+Asignar un valor se hace mediante del signo =, se debe hacer sobre una variable que ya fue declarada 
+*/
+nombre = "Markus"
+console.log(nombre + " " + apellido);
+//desde otra variable
+var edad=16
+var edadestudiante=edad
+//desde teclado
+var gradoEstudiante=prompt("Ingrese Un Grado");
+console.log("El grado es:" + gradoEstudiante);
+/* Constantes: 
+Son valores que una vez inicializados no se pueden cambiar lo que almacenan. Se crean con la palabra reservada Const, El nombre de las constantes se escriben todo en mayuscula para diferenciarlo de la variables Enter 
+*/ 
+const PI=3.1416;
+PI = 5;
+
