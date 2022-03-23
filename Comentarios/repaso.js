81fd17f30761be12ee23040fa803cb3043731a1e
MarkusyMiguel <youismyfoukingdream@gmail.com>
2022-03-23 10:26:09 -0500

    taller3

---
+++ Comentarios/repaso.js
@@ -0,0 +1,26 @@
+//Primer punto
+var metros;
+var kilomertro;
+//Creacion de variable
+var metros = prompt("por favor ingrese el valor en metros");     
+
+//realizar la operacion
+kilomertro = metros / 1000;
+
+//mostrar mensaje con resultado
+console.log (metros + "m equivalen a" + kilomertro + "km");
+
+// segundo punto
+
+var litros
+var distancia 
+
+distancia = prompt("digite la distancia dada")
+litros = (distancia/100)*290
+console.log ("El total de combustible que se gasto en el viaje fue " + litros )
+
+
+
+
+
+
---
+++ Comentarios/taller3.js
@@ -0,0 +1,61 @@
+//23|03|22
+//Andres revellon y Miguel angel martinez y markus SchÖlz :D
+//Primer punto
+var litros;
+var horas;
+
+var litros = prompt ("Digite la cantidad de litros");
+horas =  (litros / 145) ;
+
+console.log ("La cantidad de horas gastadas fueron " + horas  );
+ 
+//Punto 2
+var lonmetros ;
+var milimetros;
+
+var lonmetros = prompt (" Digite la cantidad de metros ");
+
+milimetros = lonmetros * 1000;
+
+console.log ( lonmetros + " m equivalen a " + milimetros + " ml ");
+
+//punto 3
+var decimetros;
+
+var milimetros = prompt ("digite la cantidad de milimetros");
+
+decimetros = milimetros / 100 
+
+console.log (milimetros + " ml equivalen a " + decimetros + " dm ");
+
+//punto 4
+var kilomertros;
+
+var decimetros;
+decimetros = prompt (" Digite la cantidad de decimetros");
+
+kilomertros = decimetros / 10000
+
+console.log (decimetros + " dm equivalen a " + kilomertros + " km ");
+
+//punto 5 
+var horas;
+var valor_a_pagar;
+
+horas = prompt ("Digite la duracion de la llamada");
+valor_a_pagar = 120 * 60 * horas
+console.log ("El total a pagar es $" + valor_a_pagar );
+
+//punto 6
+var hora_clase;
+var hora_adicional
+var total_pagar;
+
+hora_clase = prompt (" Digite el total de las horas que duro la clase " );
+
+total_pagar = hora_clase * 40000 
+hora_adicional = total_pagar + 25000
+
+console.log ("El total a pagar de la clase es $ " + total_pagar + "mas el coso adicional ese es" + hora_adicional );  
+
+
